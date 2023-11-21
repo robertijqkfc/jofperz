a09b2c381bc032d5dfc0101da294851cf0b6dc3a
robertijqkfc <145122023+robertijqkfc@users.noreply.github.com>
2023-11-21 03:17:41 +0100

    Add files via upload

---
+++ mission7/tp+6.js
@@ -0,0 +1,13 @@
+function calcul_moyenne(){
+    var n1=prompt("Donner la première note: ");
+    var n2=prompt("Donner la deuxième note: ");
+    var n3=prompt("Donner la troisième note: ");
+    
+    var somme = Number(n1)+Number(n2)+Number(n3);
+
+    document.write("Voici la somme: "+somme+"<br>");
+    var moyenne=somme/3
+
+    document.write("Voici la moyenne: "+moyenne+"<br>");
+}
+
